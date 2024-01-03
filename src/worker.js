aaba50372208f80206c03b7ae78af5e836291881
da99 <spam.i.hate@miniuni.com>
2024-01-03 13:47:19 -0600

    Development checkpoint.

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,10 @@
 
 export default {
-  fetch(__req, __env, __ctx) {
-    return new Response('Hello World!');
+  fetch(req, __env, __ctx) {
+    if (req.url.indexOf("low-supplies") > -1) {
+      const newURL = 'https://docs.google.com/document/d/1Xr5EH8pTicLnGFOO_PtPiCfwrX8LR_Wbn7-qTwutsOQ/edit?usp=sharing';
+      return Response.redirect(newURL, 302);
+    }
+    return new Response(`Hello World: ${req.url}`);
   }
 }
