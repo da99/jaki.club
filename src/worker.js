446c4329abc9ef3b1319c74e3daa64b9fa04082a
da99 <spam.i.hate@miniuni.com>
2024-01-17 14:06:25 -0600

    Added: 404 status.

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,10 @@
 
 export default {
   fetch(req, __env, __ctx) {
-    return new Response(`Hello World: ${req.url}`);
+    const the_url = new URL(req.url);
+    if (the_url.pathname === "/" ) {
+      return new Response(`Hello World: ${req.url}`);
+    }
+    return new Response(`Not found: ${req.url}`, { status: 404, statusText: "Not Found"});
   }
 }
