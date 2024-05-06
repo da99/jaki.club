28fa592ffd0e28af9e77760145c96d69f8f68d09
da99 <spam.i.hate@miniuni.com>
2024-05-06 07:37:34 -0500

    Added: local D1 development.

---
+++ d1/inspect.sql
@@ -0,0 +1,5 @@
+
+-- SELECT COUNT(id) as COUNT_EMAIL FROM email;
+-- SELECT COUNT(id) as COUNT_CODE FROM codes;
+
+SELECT COUNT(*) as CODE_COUNT FROM email LEFT JOIN codes ON email.id = codes.email_id;
