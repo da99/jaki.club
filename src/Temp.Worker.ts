03468c16cce9b37b4c765092a66717fa36130d78
da99 <spam.i.hate@miniuni.com>
2024-05-20 07:25:24 -0500

    Development checkpoint.

--- src/Temp.Worker.ts
+++ src/Temp.Worker.ts
@@ -1,37 +1,69 @@
 
 import { EmailMessage } from "cloudflare:email";
 // import type { ForwardableEmailMessage } from "cloudflare:email";
+
 import { createMimeMessage } from "mimetext/browser";
 import PostalMime from 'postal-mime';
 import type { Bindings, EmailMessageEvent } from '/apps/jaki.club/src/Base.mts';
 
 export default {
+
   async fetch(req: Request, _env: Bindings, _ctx: any) {
     return new Response(`Not found: ${req.url}`, { status: 404, statusText: "Not Found"});
-  },
+  }, // fetch
 
   async email(message: EmailMessageEvent, _env: Bindings, _ctx: any) {
 
-    const computer_email = 'computer@jaki.club';
+    // TRIM and UPPERCASE subject.
+    // trim and UPPERCASE to: address.
+    //
+    // check if previous code is in queue.
+    //   if yes
+    //     tell to check SPAM/inbox.
+    //       RETURN
+    //
+    //  Check how many logins in the past 1 hr.
+    //    if more than 3,
+    //      Send message, timeout for 15 more minutes.
+    //      RETURN
+    //  Generate new code.
+    //  Send code to FROM:
+    //    include message: FROM, IP ADDRESS
+    //
+    const staging_computer_email = 'computer@the-stage.jaki.club';
+
     const email = await PostalMime.parse(message.raw);
+    const subject = (email.subject || '').trim().toUpperCase();
+    const to      = (message.to).trim().toUpperCase();
+    const from    = (message.from).trim();
+
+    switch (to) {
+      case 'enter@jaki.club':
+      case 'enter@the-stage.jaki.club':
+        // Everything is ok. continue.
+        break;
+
+      default:
+        // FORWARD this message to errors @ jaki.club
+        return;
+    } // switch
 
     const msg = createMimeMessage();
     const msg_id = message.headers.get("Message-ID") || "unknown";
-    msg.setHeader("In-Reply-To", msg_id);
-    msg.setSender({ name: "The Computer", addr: computer_email });
+    msg.setHeader('In-Reply-To', msg_id);
+    msg.setSender({ name: (to.split('@')[0] || 'unknown').toUpperCase(), addr: to });
     msg.setRecipient(message.from);
     msg.setSubject(`Pong: Origin Subject: ${email.subject}`);
     msg.addMessage({
       contentType: 'text/plain',
-      data: `You wrote: ${ email.text || "unknown" }`
+      data: `You wrote: ${ email.text || '[NOTHING]' }`
     });
 
-    const replyMessage = new EmailMessage(
-      computer_email,
+    await message.reply(new EmailMessage(
+      message.to,
       message.from,
       msg.asRaw()
-    );
+    ));
+  } // email
 
-    await message.reply(replyMessage);
-  }
-}
+} // export
