135fb54f65f82b5ab10bb8d96110830bebef9534
da99 <spam.i.hate@miniuni.com>
2024-05-21 11:13:27 -0500

    Development checkpoint.

--- src/Temp.Worker.ts
+++ src/Temp.Worker.ts
@@ -3,7 +3,7 @@ import { EmailMessage } from "cloudflare:email";
 // import type { ForwardableEmailMessage } from "cloudflare:email";
 
 import { createMimeMessage } from "mimetext/browser";
-// import PostalMime from 'postal-mime';
+import PostalMime from 'postal-mime';
 import type { Bindings, EmailMessageEvent } from '/apps/jaki.club/src/Base.mts';
 
 export default {
@@ -32,8 +32,8 @@ export default {
     //
     // const staging_computer_email = 'enter@the-stage.jaki.club';
 
-    // const email = await PostalMime.parse(message.raw);
-    // const subject = (email.subject || '').trim().toUpperCase();
+    const email = await PostalMime.parse(message.raw);
+    const subject = (email.subject || '').trim().toUpperCase();
     // const to      = (message.to).trim().toUpperCase();
     // const from    = (message.from).trim();
     //
@@ -44,23 +44,23 @@ export default {
     //     break;
     // } // switch
 
-    const x = "enter@jaki.club"
+    const x = 'x@jaki.club'
     const msg = createMimeMessage();
     const msg_id = message.headers.get("Message-ID") || "unknown";
+    const created_at = message.headers.get("Date") || "unknown";
+
     msg.setHeader('In-Reply-To', msg_id);
     // msg.setSender({ name: (to.split('@')[0] || 'unknown').toUpperCase(), addr: message.to });
     msg.setSender({ name: "x", addr: x });
     msg.setRecipient(message.from);
-    msg.setSubject(`Pong: Origin Subject: {email.subject}`);
-    const new_body = `You wrote: { email.text || '[NOTHING]' }`
+    msg.setSubject(`Pong: Origin Subject: ${subject}`);
+    const new_body = `You wrote: ${subject} @ ${created_at}   ID: ${msg_id}' }`
     msg.addMessage({
       contentType: 'text/plain',
       data: new_body
     });
 
-    console.log(message.from)
-    console.log(message.to)
-    console.log(x)
+    console.log(`Subject: ${subject} -- From: ${message.from} To: ${message.to} x: ${x} ${created_at}`)
     await message.reply(new EmailMessage(
       x,
       message.from,
