b8f20ddc3e35051d9541665e3372f6908498a7ed
da99 <spam.i.hate@miniuni.com>
2024-08-05 14:58:51 -0500

    Development checkpoint.

---
+++ public/section/enter/index.mts
@@ -0,0 +1,3 @@
+
+import { body, element as e, form_post } from "/apps/da/src/www/src/html.mts";
+import { SETTINGS } from "/apps/jaki.club/src/Base.mts";
---
+++ templates/index.html.rb
@@ -0,0 +1,22 @@
+#
+require '../src/JAKI'
+
+JAKI.html5! do
+  default_head('{NAME}', "#{SETTINGS['SITE_NAME']} Homepage")
+
+  body do
+    h1 "Title does here."
+
+    main do
+      div.wait! do
+        "Content goes here."
+      end
+
+    end # main
+
+    footer { span.copyright '(c) {YEAR}. All rights reserved.' }
+    script(type: 'module', src: JAKI.static_url('/section/{NAME}/index.mjs')) { "" }
+
+  end # body
+
+end # html5
